c2b65c63413ed715eb7797d311001715163d0b27
Leah Wasser <lwasser@NEON-04193.local>
2016-02-09 15:21:43 -0700

    new build

--- code/02-spatial-data-formats.R
+++ code/02-spatial-data-formats.R
@@ -1,3 +1,12 @@
+## ----na-value-tiff, eval=FALSE-------------------------------------------
+## 
+## #if you want a NA value of -9999, then you have to specify this when you
+## #export a raster file in R
+## exampleRaster <- writeRaster(rasterObject,
+##                              FileName.tif,
+##                              datatype = "INT1U",
+##                              NAflag = -9999)
+
 ## ----elevation-map, include=TRUE, results="hide", echo=FALSE-------------
 #load libraries
 library(raster)
