2da5c2bf69771ad037f724478ff4b8348a1325c3
François Michonneau <francois.michonneau@gmail.com>
2018-03-19 17:51:20 -0400

    add ssh key

--- tic.R
+++ tic.R
@@ -19,8 +19,8 @@ if (Sys.getenv("id_rsa") != "") {
   get_stage("before_deploy") %>%
       add_step(step_setup_ssh())
 
-    get_stage("deploy") %>%
-        add_step(step_push_deploy(path = "_site", branch = "website", orphan = TRUE))
+  get_stage("deploy") %>%
+      add_step(step_push_deploy(path = "_site", branch = "gh-pages", orphan = TRUE))
 
 
 }
